3af2229cfab76a9c1caf4687756779937429bc35
MayureshMadavD <mayuresh.madav@gokwik.co>
2024-07-18 19:54:53 +0530

    global

--- server.js
+++ server.js
@@ -1,28 +1,28 @@
-import { getSheetFromData } from "./main.js"; 
 import express from "express";
-import bodyParser from  'body-parser';
+import bodyParser from "body-parser";
+import { getSheetFromData } from "./main.js";
 
-
-const app = express()
-const port = 2000
+const app = express();
+const port = 2000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+  res.send("server running");
+});
 
-app.get('/', (req, res) => {
-  res.send('server running')
-})
-
-app.post('/customerd',(req,res) =>{
- try{
+app.post("/customerd", async (req, res) => {
+  try {
     const reqs = req.body;
-    console.log(reqs)
- }catch(e){
-    console.log(e)
- }
-})
+    const dataValues = await getSheetFromData();
+    console.log(dataValues.values);
+    console.log(reqs);
+  } catch (e) {
+    console.log(e);
+  }
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
