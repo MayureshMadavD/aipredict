774233930cd75adc636934d829c49a610e7489e1
Anek Porwal (Gokwik) <anek.porwal@gokwik.co>
2024-07-18 20:29:43 +0530

    Added : Customer Specific Data based on API call.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import bodyParser from "body-parser";
 import { getSheetFromData } from "./main.js";
 
 const app = express();
-const port = 2000;
+const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -14,10 +14,12 @@ app.get("/", (req, res) => {
 
 app.post("/customerd", async (req, res) => {
   try {
-    const reqs = req.body;
+    const customer_id = req.body.customer_id;
     const dataValues = await getSheetFromData();
-    console.log(dataValues.values);
-    console.log(reqs);
+    const JSONdata = convertArrayToJSON(dataValues.values);
+    const filteredCustomerData = getObjectsByCustomerId(JSONdata, customer_id);
+    console.log(filteredCustomerData);
+
   } catch (e) {
     console.log(e);
   }
@@ -26,3 +28,39 @@ app.post("/customerd", async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+
+
+// UTILS FUNCTIONS
+
+
+// Function convert google sheet data to JSON
+function convertArrayToJSON(data) {
+  // Extract the keys from the first array
+  const keys = data[0];
+  
+
+  const result = [];
+  
+  for (let i = 1; i < data.length; i++) {
+      const values = data[i];
+      const obj = {};
+      
+      keys.forEach((key, index) => {
+          obj[key.trim()] = values[index];
+      });
+      
+      result.push(obj);
+  }
+  
+  return result;
+}
+
+// function to get filter json data based on customer id
+function getObjectsByCustomerId(data, customerId) {
+  return data.filter(item => item.customer_id === customerId);
+}
+
+
+
+
